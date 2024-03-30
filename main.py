d34177b3a22520d533d3ac1fbe36bfd7934f0384
artem <artemustyan2003@mail.ru>
2024-03-30 22:15:16 +0300

    Add image output from the camera

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+import cv2
+
+def videoDisplay():
+    # Захватываем видео с камеры
+    vebCamVideo = cv2.VideoCapture(0)
+    # Цикл вывода изображения камеры пока не нажата клавиша
+    while cv2.waitKey(1) < 0:
+        hasFrame, vebCamVideoFrame = vebCamVideo.read()
+        #Если нет изображения программа прекратит свое выполнение
+        if not hasFrame:
+            cv2.waitKey()
+            break
+        #Вывод видео с камеры
+        cv2.imshow("Name", vebCamVideoFrame)
+
+
+videoDisplay()
